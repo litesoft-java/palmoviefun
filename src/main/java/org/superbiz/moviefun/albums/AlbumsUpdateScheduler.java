e8023262208bca61ec14b5f4a1853f6df726b324
arunkumrs <arun.kumrs@yahoo.com>
2018-10-22 11:10:09 -0600

    DB Locking of background

--- src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsUpdateScheduler.java
@@ -3,9 +3,13 @@ package org.superbiz.moviefun.albums;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAsync
@@ -16,23 +20,46 @@ public class AlbumsUpdateScheduler {
     private static final long MINUTES = 60 * SECONDS;
 
     private final AlbumsUpdater albumsUpdater;
+    private final JdbcTemplate jdbcTemplate;
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater) {
+    public AlbumsUpdateScheduler(AlbumsUpdater albumsUpdater, DataSource dataSource) {
         this.albumsUpdater = albumsUpdater;
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
     @Scheduled(initialDelay = 15 * SECONDS, fixedRate = 2 * MINUTES)
     public void run() {
         try {
+            if (!startAlbumSchedulerTask()) {
+                logger.debug("Nothing to start");
+                return;
+            }
+
             logger.debug("Starting albums update");
             albumsUpdater.update();
-
             logger.debug("Finished albums update");
 
-        } catch (Throwable e) {
+        } catch (Exception e) {
             logger.error("Error while updating albums", e);
+        }
+    }
+
+    private boolean startAlbumSchedulerTask() {
+        try {
+            int updatedRows = jdbcTemplate.update(
+                    "UPDATE album_scheduler_task" +
+                            " SET started_at = now()" +
+                            " WHERE started_at IS NULL" +
+                            " OR started_at < date_sub(now(), INTERVAL 2 MINUTE)"
+            );
+
+            return updatedRows > 0;
+        } catch (DataAccessException e) {
+            logger.error("Error while claiming scheduled task", e);
+            return false;
         }
     }
 }
